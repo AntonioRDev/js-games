9b4c448dfdbcaf60cd0e630711d557fc7d9b5cd1
Antônio Carlos <antonio@beestart.com.br>
2020-04-13 08:55:18 -0300

    ..

--- memory-game/js/app.js
+++ memory-game/js/app.js
@@ -5,18 +5,18 @@ window.onload = () => {
 };
 
 const cardArray = [
-  { name: "sutia", img: "../images/sutia.svg" },
-  { name: "sutia", img: "../images/sutia.svg" },
-  { name: "bikini", img: "../images/bikini.svg" },
-  { name: "bikini", img: "../images/bikini.svg" },
-  { name: "chapeu", img: "../images/chapeu.svg" },
-  { name: "chapeu", img: "../images/chapeu.svg" },
-  { name: "top", img: "../images/top.svg" },
-  { name: "top", img: "../images/top.svg" },
-  { name: "meia", img: "../images/meia.svg" },
-  { name: "meia", img: "../images/meia.svg" },
-  { name: "acessorio", img: "../images/acessorio.svg" },
-  { name: "acessorio", img: "../images/acessorio.svg" },
+  { name: "sutia", img: "images/sutia.svg" },
+  { name: "sutia", img: "images/sutia.svg" },
+  { name: "bikini", img: "images/bikini.svg" },
+  { name: "bikini", img: "images/bikini.svg" },
+  { name: "chapeu", img: "images/chapeu-coco.svg" },
+  { name: "chapeu", img: "images/chapeu-coco.svg" },
+  { name: "top", img: "images/esportes-e-competicao.svg" },
+  { name: "top", img: "images/esportes-e-competicao.svg" },
+  { name: "meia", img: "images/meias.svg" },
+  { name: "meia", img: "images/meias.svg" },
+  { name: "acessorio", img: "images/acessorio.svg" },
+  { name: "acessorio", img: "images/acessorio.svg" },
 ];
 
 const createBoard = () => {
@@ -28,8 +28,14 @@ const createBoard = () => {
     card.setAttribute("height", 100);
     card.setAttribute("data-id", i);
     container.appendChild(card);
-    // card.addEventListener("click", flipCard);
+    card.addEventListener("click", () => flipCard(card));
   }
 
   console.log("create board");
 };
+
+function flipCard(card) {
+  const cardId = card.getAttribute("data-id");
+  const cardData = cardArray[cardId];
+  card.setAttribute("src", cardData.img);
+}
